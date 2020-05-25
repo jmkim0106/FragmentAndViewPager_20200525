9add9d4433fd6d3863f2d9518e4d4771c2778325
weekyeon <weekyeon@gmail.com>
2020-05-25 18:08:36 +0900

    어댑터 객체화 + 뷰페이저와 연결

--- app/src/main/java/com/example/fragmentandviewpager_20200525/MainActivity.java
+++ app/src/main/java/com/example/fragmentandviewpager_20200525/MainActivity.java
@@ -5,11 +5,14 @@ import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import com.example.fragmentandviewpager_20200525.adapters.MyViewPagerAdapter;
 import com.example.fragmentandviewpager_20200525.databinding.ActivityMainBinding;
 
 public class MainActivity extends BaseActivity {
 
     ActivityMainBinding binding;
+
+    MyViewPagerAdapter mvpa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +29,7 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setValues() {
-
+        mvpa = new MyViewPagerAdapter(getSupportFragmentManager());
+        binding.myViewPager.setAdapter(mvpa);
     }
 }
